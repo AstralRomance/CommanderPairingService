e2f4421eb050fb19e0a930221ad5e4bd4acfa146
AstralRomance <andrdoncwork@mail.ru>
2021-02-20 12:46:26 +0300

    Add players output.

--- commanderpairings/src/frontend.js
+++ commanderpairings/src/frontend.js
@@ -1,12 +1,23 @@
+function getToken()
+{
+    fetch('/token', {method:'GET'})
+            .then(response => response.json())
+            .then(json => json.jwt)
+            .then(token => {
+                localStorage.setItem('jwt',token);
+                return token;
+            })
+}
+
 function checkToken()
 {
     if (localStorage.getItem('jwt') != null)
             fetch('/validate_token', {headers:
-                                                    {
-                                                        'Authorization':localStorage.getItem('jwt'),
-                                                        'Content-Type': 'application/json'
-                                                    },
-                                                    method:'GET'})
+                                            {
+                                                'Authorization':localStorage.getItem('jwt'),
+                                                'Content-Type': 'application/json'
+                                            },
+                                    method:'GET'})
             .then(response => response.json())
             .then(json => json.is_valid)
             .then(validate => {
@@ -19,17 +30,6 @@ function checkToken()
             getToken()
 }
 
-function getToken()
-{
-    fetch('/token', {method:'GET'})
-            .then(response => response.json())
-            .then(json => json.jwt)
-            .then(token => {
-                localStorage.setItem('jwt',token);
-                return token;
-            })
-}
-
 function makePairingsPage()
 {
     fetch('/get_pairings', {method:'GET', headers:
@@ -39,11 +39,13 @@ function makePairingsPage()
     .then(response => response.json())
     .then(json => json.players)
     .then(players => {
+        let html_container = document.getElementById('playerName')
         for (let player of players)
         {
             let player_container = document.createElement('div');
             player_container.className = 'PlayerContainer';
             player_container.append(document.createTextNode(player[0]));
+            html_container.append(player_container);
         }
     })
 }
