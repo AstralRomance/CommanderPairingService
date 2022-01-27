4aec47fc4d423fcfc4e3307a69d7e3e9cd2147a5
NikitaCartes <nikich98@yandex.ru>
2022-01-28 00:17:50 +0300

    Fix #3

--- src/pairings/tables.py
+++ src/pairings/tables.py
@@ -1,9 +1,15 @@
+import uuid
+
+
 class Event:
 
-    def __init__(self, name, date, event_id, players, rounds, is_finished=True):
+    def __init__(self, name, date, players, rounds, event_id=None, is_finished=True):
         self.name = name
         self.date = date
-        self.id = event_id
+        if event_id is None:
+            self.id = str(uuid.uuid4())
+        else:
+            self.id = event_id
         self.players = players
         self.rounds = rounds
         self.is_finished = is_finished
